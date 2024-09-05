7f7130628537af5888b72b4b5a292d5c6ec0a83b
AtharvaJoshi1302 <110755376+AtharvaJ1302@users.noreply.github.com>
2024-09-05 08:49:57 +0530

    minor changes

--- lib/screens/login/login_controller.dart
+++ lib/screens/login/login_controller.dart
@@ -1,4 +1,5 @@
 import 'package:bakery_app/constants/constants.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class LoginController extends GetxController{
@@ -14,7 +15,7 @@ class LoginController extends GetxController{
       Get.toNamed(RouteConstants.otpScreen); // Replace '/otp' with the actual route to your OTP page
     } else {
       // Show error message
-      Get.snackbar('Error', 'Please provide a valid number');
+      Get.snackbar('Error', 'Please provide a valid number',colorText: Colors.white,backgroundColor: Colors.red,snackPosition: SnackPosition.BOTTOM);
     }
   }
 }

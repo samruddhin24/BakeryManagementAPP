dfc466a18bb6b303a53f2f200f284500b557e9db
AtharvaJoshi1302 <110755376+AtharvaJ1302@users.noreply.github.com>
2024-09-11 19:23:47 +0530

    minor changes

--- lib/screens/registration/registration_screen.dart
+++ lib/screens/registration/registration_screen.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class RegistrationScreen extends StatelessWidget {
   const RegistrationScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold();
   }
 }
